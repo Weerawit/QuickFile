cddde5889253d82583c6797c600cc6a58721f193
Weerawit Maneepongsawat <weerawit@WMs-MacBook-Pro.local>
2014-11-20 19:25:08 +0700

    Initial Commit

---
+++ QuickFile/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickFile
+//
+//  Created by Weerawit Maneepongsawat on 11/20/2557 BE.
+//  Copyright (c) 2557 Weerawit Maneepongsawat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuickFile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuickFile
+//
+//  Created by Weerawit Maneepongsawat on 11/20/2557 BE.
+//  Copyright (c) 2557 Weerawit Maneepongsawat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
