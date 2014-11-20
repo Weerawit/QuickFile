218c1ecee22b94c14eae9423363fdda0fd490dcd
Weerawit Maneepongsawat <weerawit@WMs-MacBook-Pro.local>
2014-11-20 20:29:42 +0700

    init

--- QuickFile/ViewController.swift
+++ QuickFile/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Weerawit Maneepongsawat on 11/20/2557 BE.
 //  Copyright (c) 2557 Weerawit Maneepongsawat. All rights reserved.
 //
-
+import Foundation
 import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var fileNameTxt: NSTextField!
+    @IBOutlet weak var fileExtensionTxt: NSTextField!
+    @IBOutlet weak var appendSeqChk: NSButton!
+    @IBOutlet weak var openInEditorChk: NSButton!
+    @IBOutlet weak var editorRadio: NSMatrix!
+    
+    let userDefault: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
     }
 
     override var representedObject: AnyObject? {
@@ -23,5 +32,11 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func restoreDefault(sender: AnyObject) {
+        println("default press")
+        
+        userDefault.setValue("Untitle", forKey: "filename")
+        
+    }
 }
 
